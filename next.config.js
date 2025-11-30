315374824ab1db33a5dc62838ab1509df8d74ec8
pckerneis <pc.kerneis@gmail.com>
2025-11-30 21:40:29 +0100

    Fix basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repoBasePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  ...(repoBasePath
+    ? {
+        basePath: `/${repoBasePath}`,
+        assetPrefix: `/${repoBasePath}/`,
+      }
+    : {}),
 };
 
 module.exports = nextConfig;
